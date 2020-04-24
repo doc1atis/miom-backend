8c668e0797713b9caa878c16819d8ff5962504a8
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-04-23 22:58:34 -0400

    fix url

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
   res.json({ olgy: "I receive olgy" });
 });
 module.exports = router;
